066d770dc4b6e9c221cd3784e525116b42b77ae2
yonikim <hoyeon.kim2773@gmail.com>
2021-05-14 22:43:24 +0900

    백준 알고리즘 공부

--- Baekjoon/TEST/WEAK/weak.py
+++ Baekjoon/TEST/WEAK/weak.py
@@ -3,7 +3,7 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-def m_14888():
+def m_14888(): ### 재귀탐색
     maxv = -sys.maxsize - 1
     minv = sys.maxsize
     n = int(input())
@@ -28,7 +28,7 @@ def m_14888_recur(num, idx, add, sub, multi, division, n, lst, maxv, minv):
             m_14888_recur(num // lst[idx], idx + 1, add - 1, sub, multi, division, n, lst, maxv, minv)
 
 
-def m_2504():
+def m_2504(): ### 스택
     sb = list(input())
     # print(sb)
     stack = list()
@@ -84,7 +84,7 @@ def m_2504():
     pass 
 
 
-def m_14719():
+def m_14719(): ### 시뮬레이션
     h, w = map(int, input().split())
     drop = list(map(int, input().split()))
     raindrop = 0
@@ -98,8 +98,112 @@ def m_14719():
         raindrop = raindrop + abs(drop[i] - which_low)
     print(raindrop)
 
+import sys
+from itertools import combinations
+
+def m_1062(): ### 완전탐색
+    n, k = map(int, input().split())
+    basic_set = {'a', 'c', 'i', 'n', 't'}
+    all_set = set()
+    word_lst = []
+    ans = 0
+    for _ in range(n):
+        word_set = set(sys.stdin.readline())
+        word_set = word_set - {'\n'}
+        print(word_set)
+        diff_set = word_set - basic_set
+        print(diff_set)
+        word_lst.append(list(diff_set))
+
+        all_set = all_set.union(word_set)
+    diff_set = all_set - basic_set
+    diff_lst = list(diff_set)
+    diff_lst = sorted(diff_lst)
+    per = combinations(diff_lst, k - 5)
+
+    for spellin in per:
+        cnt = 0
+        spell_lst = list(spellin) # 알려줄 알파벳 리스트
+        for word_gp in word_lst: #[[r], [c,a,r]] 리스트에서
+            isTrue = 1
+            for word in word_gp: # [c,a,r] -> c, 실제 단어
+                if word in spell_lst:
+                    isTrue *= 1
+                else:
+                    isTrue *= 0
+            if isTrue == 1:
+                cnt += 1
+        ans = max(ans, cnt)
+    print(ans)
+
+
+def m_1700(): ### 그리디
+    n, k = map(int, input().split())
+    multitap = list(map(int, input().split()))
+
+    plugs = []
+    count = 0
+    for i in range(k):
+        if multitap[i] in plugs:
+            continue
+        # 플러그가 비어 있으면 꼽기
+        if len(plugs) < n:
+            plugs.append(multitap[i])
+            continue
+        multitap_idxs = []
+        hasplug = True
+
+        for j in range(n):
+            if plugs[j] in multitap[i:]:
+                multitap_idx = multitap[i:].index(plugs[j])
+            else:
+                multitap_idx = 101
+                hasplug = False
+            multitap_idxs.append(multitap_idx)
+
+            if not hasplug:
+                break
+        
+        # 플러그를 뽑는다
+        plug_out = multitap_idxs.index(max(multitap_idxs))
+        del plugs[plug_out]
+        plugs.append(multitap[i])
+        count += 1
+    print(count)
+
+
+def m_1806(): ### 투포인터
+    n, s = map(int, input().split())
+    a = list(map(int, input().split()))
     
+    sum_a = [0] * (n + 1)
+    for i in range(1, n + 1):
+        sum_a[i] = sum_a[i - 1] + a[i - 1]
+
+    answer = 1000001
+    start = 0
+    end = 1
+
+    while start != n:
+        if sum_a[end] - sum_a[start] >= s:
+            if end - start < answer:
+                answer = end - start
+            start += 1
+        else:
+            if end != n:
+                end += 1
+            else: 
+                start += 1
+
+    if answer != 100001:
+        print(answer)
+    else:
+        print(0)
+
 
 # m_14888()
 # m_2504()
-m_14719()
+# m_14719()
+# m_1062()
+# m_1700()
+m_1806()
