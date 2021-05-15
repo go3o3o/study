69e338b3a1cadbd040814d100b78eedd179ce699
yonikim <hoyeon.kim2773@gmail.com>
2021-05-15 23:12:16 +0900

    백준 알고리즘 공부

--- Baekjoon/TEST/WEAK/weak.py
+++ Baekjoon/TEST/WEAK/weak.py
@@ -200,10 +200,137 @@ def m_1806(): ### 투포인터
     else:
         print(0)
 
+import sys
+from heapq import heappush, heappop
+
+def m_1916():
+    n = int(input())
+    m = int(input())
+    bus = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        start, end, cost = map(int, input().split())
+        bus[start].append((end, cost))
+    start, end = map(int, input().split())
+
+    print(m_1916_dijkstra(start, end, n, bus))
+
+def m_1916_dijkstra(start, end, n, bus):
+    heap = []
+    heappush(heap, (0, start))
+    distance = [sys.maxsize] * (n + 1)
+    distance[start] = 0
+
+    while heap:
+        weight, index = heappop(heap)
+        for e, c in bus[index]:
+            if distance[e] > weight + c:
+                distance[e] = weight + c
+                heappush(heap, (weight + c, e))
+    return distance[end]
+
+
+def m_1197(): ### 벨만포드 뼈대 문제
+    v, e = map(int, input().split())
+    edge = []
+    for _ in range(e):
+        a, b, w = map(int, input().split())
+        edge.append((w, a, b))
+    edge.sort(key=lambda x: x[0])
+
+    parent = list(range(v + 1))
+
+    result = 0
+    for w, s, e in edge:
+        if m_1197_find(s, parent) != m_1197_find(e, parent):
+            m_1197_union(s, e, parent)
+            result += w
+    print(result)
+
+def m_1197_union(a, b, parent):
+    a = m_1197_find(a, parent)
+    b = m_1197_find(b, parent)
+
+    if b < a:
+        parent[a] = b 
+    else:
+        parent[b] = a
+
+def m_1197_find(a, parent):
+    if a == parent[a]:
+        return a
+    parent[a] = m_1197_find(parent[a], parent) 
+    return parent[a]
+
+
+def m_16916(): ### KMP 알고리즘
+    s = input()
+    p = input()
+
+    pi = [0 for x in range(len(p))]
+    if m_16916_kmp(s, p, pi):
+        print('1')
+    else:
+        print('0')
+
+def m_16916_getPI(pattern, pi):
+    j = 0
+    for i in range(1, len(pattern)):
+        while j > 0 and pattern[i] != pattern[j]:
+            j = pi[j - 1]
+        if pattern[i] == pattern[j]:
+            j += 1
+            pi[i] = j
+
+def m_16916_kmp(s, pattern, pi):
+    m_16916_getPI(pattern, pi)
+    j = 0
+    for i in range(len(s)):
+        while j > 0 and s[i] != pattern[j]:
+            j = pi[j - 1]
+        if s[i] == pattern[j]:
+            if j == len(pattern) - 1:
+                return True
+            else:
+                j += 1
+    return False
+
+
+def m_2252(): ### 위상정렬
+    n, m = map(int, input().split())
+    students = [0 for _ in range(n)]
+    heights = {}
+    for _ in range(m):
+        a, b = map(int, input().split())
+        students[b - 1] += 1
+        if a - 1 in heights:
+            heights[a - 1].append(b - 1)
+        else:
+            heights[a - 1] = [b - 1]
+
+    queue = deque()
+    for i in range(n):
+        if students[i] == 0:
+            queue.append(i)
+
+    result = []
+    while queue:
+        i = queue.popleft()
+        result.append(i + 1)
+        if i in heights:
+            for j in heights[i]:
+                students[j] -= 1
+                if students[j] == 0:
+                    queue.append(j)
+    print(*result)            
+
 
 # m_14888()
 # m_2504()
 # m_14719()
 # m_1062()
 # m_1700()
-m_1806()
+# m_1806()
+# m_1916()
+# m_1197()
+m_16916()
+# m_2252()
