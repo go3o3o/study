1562c766ee32cb13b860e1f29933c306ece143be
yonikim <hoyeon.kim2773@gmail.com>
2021-05-19 18:08:00 +0900

    구현 알고리즘

--- Baekjoon/구현/3568.py
+++ Baekjoon/구현/3568.py
@@ -1,1 +1,27 @@
 ### iSharp
+declaration = input()
+
+for idx, value in enumerate(declaration.split()):
+    if idx == 0:
+        common = value
+    else:
+        name = ''
+        first_alpha = False
+        value = value[:-1]
+        print(common, end = '')
+
+        for word in reversed(value):
+            if word == ']':
+                print('[', end='')
+            elif word == '[':
+                print(']', end='')
+            elif word.isalpha():
+                if not first_alpha:
+                    print(' ', end='')
+                    first_alpha = True
+                name = word + name
+            else:
+                print(word, end='')
+        print(name, end ='')
+        print(';')
+            
--- Baekjoon/구현/6987.py
+++ Baekjoon/구현/6987.py
@@ -1,1 +1,49 @@
 ### 월드컵
+from itertools import combinations
+import sys
+
+input = sys.stdin.readline
+
+def backtracking(k):
+    global ans 
+    if k == 15:
+        if [e for e in team[0].values()] == [0] * 3:
+            ans = 1
+        return
+    if team[game[k][0]]['win'] and team[game[k][1]]['lose']:
+        team[game[k][0]]['win'] -= 1
+        team[game[k][1]]['lose'] -= 1
+        backtracking(k + 1)
+        team[game[k][0]]['win'] += 1
+        team[game[k][1]]['lose'] += 1
+
+    if team[game[k][0]]['draw'] and team[game[k][1]]['draw']:
+        team[game[k][0]]['draw'] -= 1
+        team[game[k][1]]['draw'] -= 1
+        backtracking(k + 1)
+        team[game[k][0]]['draw'] += 1
+        team[game[k][1]]['draw'] += 1
+
+    if team[game[k][0]]['lose'] and team[game[k][1]]['win']:
+        team[game[k][0]]['lose'] -= 1
+        team[game[k][1]]['win'] -= 1
+        backtracking(k + 1)
+        team[game[k][0]]['lose'] += 1
+        team[game[k][1]]['win'] += 1
+
+def make_team(record):
+    ret = []
+    for i in range(0, 18, 3):
+        ret.append({
+            'win': record[i],
+            'draw': record[i + 1],
+            'lose': record[i + 2]
+        })
+    return ret
+
+for _ in range(4):
+    team = make_team(list(map(int, input().split())))
+    game = list(combinations(range(6), 2))
+    ans = 0
+    backtracking(0)
+    print(ans, end = ' ')
