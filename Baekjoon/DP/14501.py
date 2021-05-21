4f70845c10f6f9fc250334e80406b8b6f868c57b
yonikim <hoyeon.kim2773@gmail.com>
2021-05-22 06:04:47 +0900

    DP

--- Baekjoon/DP/14501.py
+++ Baekjoon/DP/14501.py
@@ -1,1 +1,18 @@
 ### 퇴사
+n = int(input())
+t = []
+p = []
+dp = []
+for i in range(n):
+    a, b = map(int, input().split())
+    t.append(a)
+    p.append(b)
+    dp.append(b)
+
+dp.append(0)
+for i in ragne(n - 1, -1 ,-1):
+    if t[i] + i > n:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]])
+print9dp[0]
