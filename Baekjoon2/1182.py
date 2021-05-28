b053fa3ba46a52927050c613e0184641f162a058
yonikim <hoyeon.kim2773@gmail.com>
2021-05-28 21:15:50 +0900

    백준 알고리즘 공부

--- Baekjoon2/1182.py
+++ Baekjoon2/1182.py
@@ -1,8 +1,13 @@
 ### 부분수열의 합
+from itertools import combinations
 
 n, s = map(int, input().split())
-lst = map(int, input().split())
+lst = list(map(int, input().split()))
 
-result = []
-for i in lst:
-    temp = []
+result = 0 
+for i in range(1, n + 1):
+    sub = list(combinations(lst, i))
+    for j in sub:
+        if sum(j) == s:
+            result += 1
+print(result)
