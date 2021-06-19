4dd1f57114d4a88c9bde3aa37b019314410b787e
yonikim <hoyeon.kim2773@gmail.com>
2021-06-19 20:46:32 +0900

    정렬 알고리즘

---
+++ Baekjoon2/1012_.py
@@ -0,0 +1,33 @@
+from collections import deque
+dx, dy = [1, -1, 0, 0], [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = [(x, y)]
+    while queue:
+        x, y = queue.pop(0)
+        del queue[0]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= nx < m and visitied[nx][ny] == 1:
+                visitied[nx][ny] = 0
+                queue.append((nx, ny))
+        
+
+tc = int(input())
+
+for _ in range(tc):
+    m, n, k = map(int, input().split())
+    visitied = [[0] * m for _ in range(n)] 
+
+    result = 0
+
+    for i in range(k):
+        x, y = map(int, input().split())
+        visitied[y][x] = 1 
+    for i in range(n):
+        for j in range(m):
+            if visitied[i][j] == 1:
+                bfs(i, j)
+                visitied[i][j] = 0
+                result += 1
