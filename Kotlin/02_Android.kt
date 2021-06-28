79efda67cafc1ac0deb4f41f52f0a1797a611f1c
yonikim <hoyeon.kim2773@gmail.com>
2021-06-29 08:55:24 +0900

    Android Studio Setting

---
+++ Kotlin/02_Android.kt
@@ -0,0 +1,9 @@
+/*
+1. Android Studio 설치
+2. Configure > AVD Manager
+3. Create Virtual Device
+4. Hardware: Pixel 2
+5. System Image: R (버전 11) 다운로드
+6. Settings >  About emulated device > Build number 반복 클릭 -> Developer mode 진입됨
+*/ 
+
