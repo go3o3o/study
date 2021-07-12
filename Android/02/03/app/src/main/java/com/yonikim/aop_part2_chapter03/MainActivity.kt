b9588143d51e8d6490717175780ae5190199880b
yonikim <hoyeon.kim2773@gmail.com>
2021-07-13 08:56:06 +0900

    안드로이드앱 > 비밀 다이어리

--- Android/02/03/app/src/main/java/com/yonikim/aop_part2_chapter03/MainActivity.kt
+++ Android/02/03/app/src/main/java/com/yonikim/aop_part2_chapter03/MainActivity.kt
@@ -1,11 +1,102 @@
 package com.yonikim.aop_part2_chapter03
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
+
+    private val numberPicker1: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker1)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+    private val numberPicker2: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker2)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+    private val numberPicker3: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker3)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val openButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.openButton)
+    }
+    private val changePasswordButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.changePasswordButton)
+    }
+    private var changePasswordMode = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        numberPicker1
+        numberPicker2
+        numberPicker3
+
+        openButton.setOnClickListener {
+            if (changePasswordMode) {
+                Toast.makeText(this, "비밀번호 변경 중입니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+            val passwordFromUser =
+                "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+            if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                // TODO 다이어리 페이지 작성 후에 넘겨줘야 함
+//                startActivity()
+            } else {
+                shoErrorAlertDialog()
+            }
+        }
+
+        changePasswordButton.setOnClickListener {
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+            val passwordFromUser =
+                "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+            if (changePasswordMode) {
+                // 번호를 저장하는 기능
+                passwordPreferences.edit(true) {
+                    putString("password", passwordFromUser)
+                }
+                changePasswordMode = false
+                changePasswordButton.setBackgroundColor(Color.BLACK)
+            } else {
+                // changePasswordMode 활성화
+
+                if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                    changePasswordMode = true
+                    Toast.makeText(this, "변경할 패스워드를 입력해 주세요.", Toast.LENGTH_SHORT).show()
+                    changePasswordButton.setBackgroundColor(Color.RED)
+                } else {
+                    shoErrorAlertDialog()
+                }
+            }
+        }
+    }
+
+    private fun shoErrorAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("실패")
+            .setMessage("비밀번호가 잘못되었습니다.")
+            .setPositiveButton("확인") { _, _ -> }
+            .create()
+            .show()
     }
 }
