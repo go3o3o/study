c7051c91b84d46ac11d1d20c72fe5a10ef9d3d48
yonikim <hoyeon.kim2773@gmail.com>
2021-08-07 11:54:04 +0900

    안드로이드앱 > 푸시 알림 수신기

---
+++ Android/03/01/app/src/main/java/com/yonikim/aop_part3_chapter01/MyFirebaseMessagingService.kt
@@ -0,0 +1,54 @@
+package com.yonikim.aop_part3_chapter01
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val title = message.data["title"]
+        val text = message.data["message"]
+
+        createNotificationChannel()
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        
+        NotificationManagerCompat.from(this)
+            .notify(1, notificationBuilder.build())
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        private const val CHANNEL_NAME = "Emoji Party"
+        private const val CHANNEL_DESCRIPTION = "Emoji Party 를 위한 채널"
+        private const val CHANNEL_ID = "Channel ID"
+    }
+}
