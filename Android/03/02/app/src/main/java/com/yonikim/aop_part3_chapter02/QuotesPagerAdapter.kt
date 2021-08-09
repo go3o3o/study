c6c66ea2af0c20292a5a3e652b1dd0c09df84b4d
yonikim <hoyeon.kim2773@gmail.com>
2021-08-09 21:43:07 +0900

    안드로이드앱 > 오늘의 명언

--- Android/03/02/app/src/main/java/com/yonikim/aop_part3_chapter02/QuotesPagerAdapter.kt
+++ Android/03/02/app/src/main/java/com/yonikim/aop_part3_chapter02/QuotesPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.yonikim.aop_part3_chapter02
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,22 +12,6 @@ class QuotesPagerAdapter(
     private val isNameRevealed: Boolean
 ) : RecyclerView.Adapter<QuotesPagerAdapter.QuoteViewHolder>() {
 
-    class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val quoteTextView: TextView = itemView.findViewById(R.id.quoteTextView)
-        private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
-        fun bind(quote: Quote, isNameRevealed: Boolean) {
-
-            quoteTextView.text = quote.quote
-
-            if (isNameRevealed) {
-                nameTextView.text = quote.name
-                nameTextView.visibility = View.VISIBLE
-            } else {
-                nameTextView.visibility = View.GONE
-            }
-        }
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder =
         QuoteViewHolder(
             LayoutInflater.from(parent.context)
@@ -34,9 +19,28 @@ class QuotesPagerAdapter(
         )
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-        holder.bind(quotes[position], isNameRevealed)
+        val actualPosition = position % quotes.size
+        holder.bind(quotes[actualPosition], isNameRevealed)
     }
 
-    override fun getItemCount(): Int = quotes.size
+    override fun getItemCount(): Int = Int.MAX_VALUE
+
+    class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val quoteTextView: TextView = itemView.findViewById(R.id.quoteTextView)
+        private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+
+        @SuppressLint("SetTextI18n")
+        fun bind(quote: Quote, isNameRevealed: Boolean) {
+
+            quoteTextView.text = "\"${quote.quote}\""
+
+            if (isNameRevealed) {
+                nameTextView.text = "- ${quote.name}"
+                nameTextView.visibility = View.VISIBLE
+            } else {
+                nameTextView.visibility = View.GONE
+            }
+        }
+    }
 
 }
