c7a06e62ae0314c82267f036442eb4b3a69bea5c
yonikim <hoyeon.kim2773@gmail.com>
2021-08-16 08:51:09 +0900

    안드로이드앱 > 도서 리뷰 앱

--- Android/03/04/app/src/main/java/com/yonikim/aop_part3_chapter04/MainActivity.kt
+++ Android/03/04/app/src/main/java/com/yonikim/aop_part3_chapter04/MainActivity.kt
@@ -3,8 +3,11 @@ package com.yonikim.aop_part3_chapter04
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yonikim.aop_part3_chapter04.Model.BestSellerDto
+import com.yonikim.aop_part3_chapter04.adapter.BookAdapter
 import com.yonikim.aop_part3_chapter04.api.BookService
+import com.yonikim.aop_part3_chapter04.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,9 +15,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: BookAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        initBookRecyclerView()
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://book.interpark.com")
@@ -32,25 +43,26 @@ class MainActivity : AppCompatActivity() {
                         return
                     }
                     response.body()?.let {
-                        Log.d(TAG, it.toString())
-                        it.books.forEach { book ->
-                            Log.d(TAG, book.toString())
-
-                        }
-
+                        adapter.submitList(it.books)
                     }
 
                 }
 
                 override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
-
+                    Log.d(TAG, t.toString())
                 }
             })
     }
 
+    private fun initBookRecyclerView() {
+        adapter = BookAdapter()
+        binding.bookRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.bookRecyclerView.adapter = adapter
+    }
+
     companion object {
         private const val API_KEY =
-            "EB9D1F31B2F7131171B01AB2B7CE6D91B12B1F45717C970717E83F0FE388EF65"
+            "5FEF336D06B18F8E70956C4CB68069F7710D5A07BFE9262A4FA467B7A2B75F8F"
         private const val TAG = "MainActivity"
     }
 }
