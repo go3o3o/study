6b01aabb738bd5470320b195be6e7cd280953bbe
yonikim <hoyeon.kim2773@gmail.com>
2021-08-23 22:16:47 +0900

    안드로이드앱 > 틴더

---
+++ Android/03/05/app/src/main/java/com/yonikim/aop_part3_chapter05/CardItem.kt
@@ -0,0 +1,6 @@
+package com.yonikim.aop_part3_chapter05
+
+data class CardItem(
+    val userId: String,
+    var name: String
+)
--- Android/03/05/app/src/main/java/com/yonikim/aop_part3_chapter05/LoginActivity.kt
+++ Android/03/05/app/src/main/java/com/yonikim/aop_part3_chapter05/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.yonikim.aop_part3_chapter05
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -7,22 +8,32 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         auth = Firebase.auth
+        callbackManager = CallbackManager.Factory.create()
 
         initLoginButton()
         initSignUpButton()
+        initFacebookLoginButton()
 
         initEmailAndPasswordEditText()
     }
@@ -36,7 +47,7 @@ class LoginActivity : AppCompatActivity() {
             auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        finish()
+                        handleSuccessLogin()
                     } else {
                         Toast.makeText(
                             this,
@@ -58,7 +69,8 @@ class LoginActivity : AppCompatActivity() {
                 .addOnCompleteListener(this) { task ->
 
                     if (task.isSuccessful) {
-                        Toast.makeText(this, "회원가입에 성공했습니다. 로그인 버튼을 눌러주세요.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "회원가입에 성공했습니다. 로그인 버튼을 눌러주세요.", Toast.LENGTH_SHORT)
+                            .show()
                     } else {
                         Log.d("LoginActivity", "signInWithEmail:failure", task.exception)
                         Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
@@ -84,8 +96,41 @@ class LoginActivity : AppCompatActivity() {
             loginButton.isEnabled = enable
             signUpButton.isEnabled = enable
         }
+    }
 
+    private fun initFacebookLoginButton() {
+        val facebookLoginButton = findViewById<LoginButton>(R.id.facebookLoginButton)
 
+        facebookLoginButton.setPermissions("email", "public_profile")
+        facebookLoginButton.registerCallback(
+            callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult) {
+                    // 페이스북 로그인 성공
+                    val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                    auth.signInWithCredential(credential)
+                        .addOnCompleteListener(this@LoginActivity) { task ->
+                            if (task.isSuccessful) {
+                                handleSuccessLogin()
+                            } else {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "페이스북 로그인에 실패했습니다.",
+                                    Toast.LENGTH_SHORT
+                                )
+                                    .show()
+                            }
+                        }
+                }
+
+                override fun onCancel() {}
+
+                override fun onError(error: FacebookException?) {
+                    Toast.makeText(this@LoginActivity, "페이스북 로그인에 실패했습니다.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            })
     }
 
     private fun getInputEmail(): String {
@@ -96,5 +141,25 @@ class LoginActivity : AppCompatActivity() {
         return findViewById<EditText>(R.id.passwordEditText).text.toString()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun handleSuccessLogin() {
+        if (auth.currentUser == null) {
+            Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val userId = auth.currentUser?.uid.orEmpty()
+        val currentUserDB = Firebase.database.reference.child("Users").child(userId)
+
+        val user = mutableMapOf<String, Any>()
+        user["userId"] = userId
+        currentUserDB.updateChildren(user)
+
+        finish()
+    }
 
 }
