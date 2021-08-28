4b0bb757b36b11f6b751e623030cc617747f002c
yonikim <hoyeon.kim2773@gmail.com>
2021-08-28 17:25:23 +0900

    안드로이드앱 > 중고거래 앱

---
+++ Android/03/06/app/src/main/java/com/yonikim/aop_part3_chapter06/DBKey.kt
@@ -0,0 +1,7 @@
+package com.yonikim.aop_part3_chapter06
+
+class DBKey {
+    companion object {
+        const val DB_ARTICLES = "Articles"
+    }
+}
--- Android/03/06/app/src/main/java/com/yonikim/aop_part3_chapter06/home/HomeFragment.kt
+++ Android/03/06/app/src/main/java/com/yonikim/aop_part3_chapter06/home/HomeFragment.kt
@@ -1,17 +1,52 @@
 package com.yonikim.aop_part3_chapter06.home
 
+import android.content.Intent
 import android.os.Bundle
-import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.yonikim.aop_part3_chapter06.DBKey.Companion.DB_ARTICLES
 import com.yonikim.aop_part3_chapter06.R
 import com.yonikim.aop_part3_chapter06.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private lateinit var articleAdapter: ArticleAdapter
+    private lateinit var articleDB: DatabaseReference
+
+    private val articleList = mutableListOf<ArticleModel>()
+    private val listener = object : ChildEventListener {
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            Log.d("HomeFragment", "onChildAdded")
+            val articleModel = snapshot.getValue(ArticleModel::class.java)
+            Log.d("HomeFragment", articleModel.toString())
+            articleModel ?: return
+
+            articleList.add(articleModel)
+            articleAdapter.submitList(articleList)
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+        override fun onChildRemoved(snapshot: DataSnapshot) {}
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+        override fun onCancelled(error: DatabaseError) {}
+
+    }
 
     private var binding: FragmentHomeBinding? = null
-    private lateinit var articleAdapter: ArticleAdapter
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,14 +54,44 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
         val fragmentHomeBinding = FragmentHomeBinding.bind(view)
         binding = fragmentHomeBinding
 
+        articleDB = Firebase.database.reference.child(DB_ARTICLES)
         articleAdapter = ArticleAdapter()
-        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
-            add(ArticleModel("0", "TITLE", 100000, "5000원", ""))
-            add(ArticleModel("1", "TITLE2", 200000, "10000원", ""))
-        })
+
+        Log.d("HomeFragment", "onViewCreated")
 
         fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
         fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+
+        fragmentHomeBinding.addArticleFloatingButton.setOnClickListener {
+            context?.let {
+                // TODO 로그인 기능 구현 후 주석 지우기
+//                if (auth.currentUser != null) {
+                    val intent = Intent(it, AddArticleActivity::class.java)
+                    startActivity(intent)
+//                } else {
+//                    Snackbar.make(view, "로그인이 필요합니다.", Snackbar.LENGTH_LONG).show()
+//                }
+
+            }
+        }
+
+        articleDB.addChildEventListener(listener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("HomeFragment", "onResume")
+
+        articleAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        Log.d("HomeFragment", "onDestroyView")
+        articleDB.removeEventListener(listener)
+
     }
 
 
