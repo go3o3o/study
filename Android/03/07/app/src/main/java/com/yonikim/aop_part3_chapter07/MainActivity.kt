5b06c9d704bc6a35e9b3884319751bfe89f13745
yonikim <hoyeon.kim2773@gmail.com>
2021-08-29 17:40:19 +0900

    안드로이드앱 > 에어비엔비

---
+++ Android/03/07/app/src/main/java/com/yonikim/aop_part3_chapter07/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.yonikim.aop_part3_chapter07
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
