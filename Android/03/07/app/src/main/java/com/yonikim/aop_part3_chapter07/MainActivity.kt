de26acb77d2308c29d766cca0ca97bd02823f668
yonikim <hoyeon.kim2773@gmail.com>
2021-08-30 18:03:21 +0900

    안드로이드앱 > 에어비엔비

--- Android/03/07/app/src/main/java/com/yonikim/aop_part3_chapter07/MainActivity.kt
+++ Android/03/07/app/src/main/java/com/yonikim/aop_part3_chapter07/MainActivity.kt
@@ -2,13 +2,73 @@ package com.yonikim.aop_part3_chapter07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var naverMap: NaverMap
+    private val mapView: MapView by lazy {
+        findViewById(R.id.mapView)
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mapView.onCreate(savedInstanceState)
 
+        mapView.getMapAsync(this)
+    }
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
 
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.497, 127.0254))
+        naverMap.moveCamera(cameraUpdate)
+
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+
 }
