4083bf2859f21426099f8b76b13deb394a8d0fa0
yonikim <hoyeon.kim2773@gmail.com>
2021-09-07 08:29:10 +0900

    안드로이드앱 > 유튜브

---
+++ Android/04/01/app/src/main/java/com/yonikim/aop_part4_chapter01/model/VideoModel.kt
@@ -0,0 +1,9 @@
+package com.yonikim.aop_part4_chapter01.model
+
+data class VideoModel(
+    val title: String,
+    val source: String,
+    val subtitle: String,
+    val thumb: String,
+    val description: String
+)
---
+++ Android/04/01/app/src/main/java/com/yonikim/aop_part4_chapter01/service/VideoService.kt
@@ -0,0 +1,5 @@
+package com.yonikim.aop_part4_chapter01.service
+
+interface VideoService {
+    
+}
