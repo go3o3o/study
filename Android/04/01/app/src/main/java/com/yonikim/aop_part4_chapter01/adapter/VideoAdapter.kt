0575afdc0d3740d3a27a0d9b60ae76d0a0b9c0f1
yonikim <hoyeon.kim2773@gmail.com>
2021-09-08 08:34:42 +0900

    안드로이드앱 > 유튜브

---
+++ Android/04/01/app/src/main/java/com/yonikim/aop_part4_chapter01/adapter/VideoAdapter.kt
@@ -0,0 +1,56 @@
+package com.yonikim.aop_part4_chapter01.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.yonikim.aop_part4_chapter01.R
+import com.yonikim.aop_part4_chapter01.model.VideoModel
+
+class VideoAdapter : ListAdapter<VideoModel, VideoAdapter.ViewHolder>(diffUtil) {
+
+    inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(item: VideoModel) {
+            val titleTextView = view.findViewById<TextView>(R.id.titleTextView)
+            val subtitleTextView = view.findViewById<TextView>(R.id.subtitleTextView)
+            val thumbnailImageView = view.findViewById<ImageView>(R.id.thumbnailImageView)
+
+            titleTextView.text = item.title
+            subtitleTextView.text = item.subtitle
+
+            Glide.with(thumbnailImageView.context)
+                .load(item.thumb)
+                .into(thumbnailImageView)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_video, parent, false)
+        return ViewHolder(inflater)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        return holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<VideoModel>() {
+            override fun areItemsTheSame(oldItem: VideoModel, newItem: VideoModel): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: VideoModel, newItem: VideoModel): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+
+}
--- Android/04/01/app/src/main/java/com/yonikim/aop_part4_chapter01/service/VideoService.kt
+++ Android/04/01/app/src/main/java/com/yonikim/aop_part4_chapter01/service/VideoService.kt
@@ -1,5 +1,11 @@
 package com.yonikim.aop_part4_chapter01.service
 
+import com.yonikim.aop_part4_chapter01.dto.VideoDto
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface VideoService {
-    
+    @GET("/v3/9dc401ea-e0b5-4313-ac19-01695a183ea7")
+    fun listVideos(): Call<VideoDto>
+
 }
