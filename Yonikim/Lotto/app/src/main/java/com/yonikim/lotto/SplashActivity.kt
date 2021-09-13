bb5e67b7b7c5fce47b41a768df7001584ce3f4dd
yonikim <hoyeon.kim2773@gmail.com>
2021-09-14 08:26:28 +0900

    안드로이드앱 > 위치검색 지도 앱

---
+++ Yonikim/Lotto/app/src/main/java/com/yonikim/lotto/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.yonikim.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashAcitivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_acitivty)
+    }
+}
