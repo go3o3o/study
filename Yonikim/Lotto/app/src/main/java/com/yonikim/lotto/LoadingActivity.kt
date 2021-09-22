c4b10fb35be55f416b025eb9b0825278613de4c6
yonikim <hoyeon.kim2773@gmail.com>
2021-09-22 10:33:44 +0900

    안드로이드앱 > 깃헙 레포지토리 앱

--- Yonikim/Lotto/app/src/main/java/com/yonikim/lotto/LoadingActivity.kt
+++ Yonikim/Lotto/app/src/main/java/com/yonikim/lotto/LoadingActivity.kt
@@ -5,19 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import com.airbnb.lottie.LottieAnimationView
 
-class SplashActivity : AppCompatActivity() {
+class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_loading)
 
 
         val handler = Handler(Looper.getMainLooper())
         val runnable = Runnable {
             goMainActivity()
         }
-        handler.postDelayed(runnable, 3000)
+        handler.postDelayed(runnable, 5000)
 
         val animationView = findViewById<LottieAnimationView>(R.id.animationView)
         animationView.setOnClickListener {
@@ -25,6 +26,10 @@ class SplashActivity : AppCompatActivity() {
             goMainActivity()
         }
 
+        val appNameTextView = findViewById<TypingWriter>(R.id.appNameTextView)
+        appNameTextView.setText("")
+        appNameTextView.setCharacterDelay(150)
+        appNameTextView.animateText(resources.getString(R.string.app_name))
     }
 
     private fun goMainActivity() {
