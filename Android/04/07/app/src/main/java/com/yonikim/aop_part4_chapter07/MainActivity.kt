94e5c35c75d18a184f4c9112984f3fa2c79ec3a0
yonikim <hoyeon.kim2773@gmail.com>
2021-09-29 09:17:10 +0900

    안드로이드앱 > 우리 동네 미세먼지

---
+++ Android/04/07/app/src/main/java/com/yonikim/aop_part4_chapter07/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yonikim.aop_part4_chapter07
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
