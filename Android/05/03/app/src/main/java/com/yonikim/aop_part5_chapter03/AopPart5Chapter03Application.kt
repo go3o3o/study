75cd37f8b97e0c8dddf3d1fb35570d14a940853b
yonikim <hoyeon.kim2773@gmail.com>
2021-10-16 13:41:53 +0900

    안드로이드앱 > 카메라 앱

---
+++ Android/05/03/app/src/main/java/com/yonikim/aop_part5_chapter03/AopPart5Chapter03Application.kt
@@ -0,0 +1,10 @@
+package com.yonikim.aop_part5_chapter03
+
+import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
+
+class AopPart5Chapter03Application : Application(), CameraXConfig.Provider {
+
+    override fun getCameraXConfig(): CameraXConfig = Camera2Config.defaultConfig()
+}
